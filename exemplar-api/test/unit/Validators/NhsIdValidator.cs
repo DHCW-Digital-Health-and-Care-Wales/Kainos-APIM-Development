70cc822d6fbedbf765cbdc8604d99da08d5d6b1b
John Stabler <john.stabler@gmail.com>
2025-08-07 10:36:59 +0100

    Added NHS number validator test

---
+++ exemplar-api/test/unit/Validators/NhsIdValidator.cs
@@ -0,0 +1,25 @@
+﻿using DHCW.PD.Validators;
+
+namespace UnitTests.Validators
+{
+    public class NhsIdValidatorTest
+    {
+
+        NhsIdValidator _validator;
+
+        public NhsIdValidatorTest()
+        {
+            _validator = new NhsIdValidator();
+        }
+
+        public void Validate_NhsId_Valid()
+        {
+            // Arrange
+            var nhsId = "4857773456";
+            // Act
+            var result = _validator.IsValid(nhsId);
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
